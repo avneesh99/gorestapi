a75495b797e6ba965d977c522a6a353c5b2a1d65
Avneesh Hota <iamavneesh.hota@gmail.com>
2021-11-30 04:16:09 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/akto-api-security/restapi
+
+go 1.17
+
+replace github.com/akto-api-security/gomiddleware => ../gomiddleware
+
+require (
+	github.com/akto-api-security/gomiddleware v0.0.0-00010101000000-000000000000
+	github.com/gorilla/mux v1.8.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/segmentio/kafka-go v0.4.23 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/akto-api-security/gomiddleware"
+	"github.com/gorilla/mux"
+)
+
+// book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+func main() {
+	// Init Router
+	r := mux.NewRouter()
+	config, _ := gomiddleware.GetConfigFromDashboard("http://localhost:8080")
+	kafkaWriter := gomiddleware.GetKafkaWriter("172.20.0.4:9092", "akto.api.logs", 100, 1*time.Second)
+	r.Use(gomiddleware.Middleware(kafkaWriter, config, 1111))
+
+	books = append(books, Book{ID: "1", Isbn: "3223", Title: "Book 1", Author: &Author{
+		Firstname: "Avneesh", Lastname: "Hota"}})
+	books = append(books, Book{ID: "2", Isbn: "2323", Title: "Book 2", Author: &Author{
+		Firstname: "Ankush", Lastname: "Jain"}})
+
+	// Route endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books", getBooks).Methods("POST")
+	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/cars", getBooks).Methods("GET")
+	r.HandleFunc("/api/auth/signin", signIn).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+// init books var as slice Book struct
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(books)
+}
+
+func signIn(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode("wefwe")
+}
